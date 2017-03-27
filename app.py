7135badfb2415c1ec8320b0086d73e8e43ed50f2
Fran Lozano <franloza@ucm.es>
2017-03-27 12:35:30 +0200

    Added first two patients

---
+++ app.py
@@ -0,0 +1,74 @@
+import pandas as pd
+import os
+import sys
+
+# Add modules path
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+from model.Translator import Translator
+from model.DecisionTree import DecisionTree
+import preprocessor as pp
+
+# Add modules path
+module_path = os.path.abspath(os.path.join('..'))
+if module_path not in sys.path:
+    sys.path.append(module_path)
+
+
+def app(filepath, max_impurity=0, language="es", output_path=None):
+    # Define translator functions
+    translator = Translator(language)
+    to_lang = translator.translate_to_language
+    to_col = translator.translate_to_column
+
+    # Load data
+    raw_data = pd.read_csv(filepath, header=0, skiprows=1, delimiter="\t", index_col=0, usecols=list(range(0, 9)),
+                           parse_dates=to_lang(["Datetime"]), decimal=",",
+                           date_parser=lambda x: pd.to_datetime(x, format="%Y/%m/%d %H:%M"))
+    # Translate column names
+    raw_data.columns = (to_col(raw_data.columns))
+
+    # Divide in blocks, extend dataset and clean data
+    block_data = pp.define_blocks(raw_data)
+    cleaned_block_data = pp.clean_processed_data(block_data)
+    extended_data = pp.extend_data(cleaned_block_data)
+    cleaned_extended_data = pp.clean_extended_data(extended_data)
+
+    # Create decision trees
+    [data, labels] = pp.prepare_to_decision_trees(cleaned_extended_data)
+    hyper_dt = DecisionTree(data, labels["Hyperglycemia_Diagnosis_Next_Block"])
+    hypo_dt = DecisionTree(data, labels["Hypoglycemia_Diagnosis_Next_Block"])
+    severe_dt = DecisionTree(data, labels["Severe_Hyperglycemia_Diagnosis_Next_Block"])
+
+    # Generate graph images
+    if output_path is not None:
+        hyper_dt.graph.write_png(output_path + '/Hyperglycemia_Tree.png')
+        hypo_dt.graph.write_png(output_path + '/Hypoglycemia_Tree.png')
+        severe_dt.graph.write_png(output_path + '/Severe_Hyperglycemia_Tree.png')
+
+    # Print patterns
+    terms = translator.translate_to_language(['Hyperglycemia_Patterns', 'Hypoglycemia_Patterns',
+                                              'Severe_Hyperglycemia_Patterns'])
+
+    # Hyperglycemia patterns
+    patterns = hyper_dt.get_patterns(max_impurity=max_impurity)
+    if patterns:
+        print('{0}'. format(terms[0].center(50, '=')))
+        for pattern in patterns:
+            print(pattern, end='\n\n')
+
+    # Hypoglycemia patterns
+    patterns = hypo_dt.get_patterns(max_impurity=max_impurity)
+    if patterns:
+        print('{0}'.format(terms[1].center(50, '=')))
+        for pattern in patterns:
+            print(pattern, end='\n\n')
+
+    # Severe Hyperglycemia patterns
+    patterns = severe_dt.get_patterns(max_impurity=max_impurity)
+    if patterns:
+        print('{0}'.format(terms[1].center(50, '=')))
+        for pattern in patterns:
+            print(pattern, end='\n\n')
--- model/DecisionTree.py
+++ model/DecisionTree.py
@@ -1,12 +1,16 @@
 from sklearn.tree import DecisionTreeClassifier
 from sklearn.model_selection import cross_val_score
 from visualization import generate_graph_tree
-from numpy import mean
+from model.Rule import Rule
+from model.Pattern import Pattern
+from model.Translator import Translator
+import numpy as np
+
 
 class DecisionTree:
     """Class that contains a model created by a decision tree and provides functions over it"""
 
-    def __init__(self, data, label, min_percentage_label_leaf=0.1, max_depth=5):
+    def __init__(self, data, label, min_percentage_label_leaf=0.1, max_depth=5, translator=Translator()):
 
         """Constructor for DecisionTree"""
         if data.empty:
@@ -29,6 +33,7 @@ class DecisionTree:
                                               min_impurity_split=1e-07,
                                               class_weight=None,
                                               presort=True)
+        self.__translator = translator
 
         # Train model
         self.__model.fit(data, label)
@@ -49,9 +54,95 @@ class DecisionTree:
         return self.__model.tree_
 
     def cross_val_score(self, scoring, cv=10):
-        return mean(cross_val_score(self.__model, self.data, self.label, scoring=scoring, cv=cv, n_jobs=-1))
+        return np.mean(cross_val_score(self.__model, self.data, self.label, scoring=scoring, cv=cv, n_jobs=-1))
+
+    def get_patterns(self, max_impurity=0.5, min_sample_size=0):
+        """
+        Function that return a list of Patterns. It allows to filter by impurity a sample size
+
+        :param max_impurity: Maximum impurity of the pattern (0 - 0.5)
+        :param min_sample_size: Minimum percentage of samples with regard to the number of records of the dataset
+        :return:  List of Pattern objects
+        """
+        n_nodes = self.tree.node_count
+        children_left = self.tree.children_left
+        children_right = self.tree.children_right
+        num_records = self.tree.n_node_samples[0]
+
+        are_leaves = np.zeros(shape=n_nodes, dtype=bool)
+        stack = []
+        decisions = []
+
+        self.__get_decisions(children_left, children_right, decisions, stack, 0, are_leaves)
+        for id_node, node in enumerate(decisions):
+            for idx_decision, decision in enumerate(decisions[id_node]):
+                if 'Feature' in decisions[id_node][idx_decision]:
+                    feature_idx = decisions[id_node][idx_decision]['Feature']
+                    if isinstance(feature_idx, int):
+                        value = self.data.columns.values[feature_idx]
+                        decisions[id_node][idx_decision]['Feature'] = value
+        leaves = []
+        for idx, leaf in enumerate(are_leaves):
+            if leaf:
+                leaves.append(decisions[idx])
+        patterns = []
+        # Last element contains the leaf information
+        valid_leaves = [leaf for leaf in leaves if leaf[-1]['Class'] == 'Positive']
+        for leaf in valid_leaves:
+            rules = [Rule(node["Feature"], node["Operator"], node["Threshold"], self.__translator) for node in
+                     leaf[:-1]]
+            pattern = Pattern(rules, num_records, leaf[-1]["Samples"], leaf[-1]["Impurity"], leaf[-1]["Number_Pos"],
+                              leaf[-1]["Number_Neg"])
+            if pattern.sample_size > min_sample_size and pattern.impurity < max_impurity:
+                patterns.append(pattern)
+
+        return patterns
 
     def __generate_graph(self):
-        label_name = self.label.name.replace('_',' ')
+        label_name = self.label.name.replace('_', ' ')
         binary_labels = ["No " + label_name, label_name]
         self.__graph = generate_graph_tree(self.__model, self.data.columns, binary_labels)
+
+    def __get_decisions(self, children_left, children_right, decisions, stack, node_id, are_leaves):
+        """
+        Recursive function that insert in a list all the decisions of every node in the tree
+        
+        :param children_left: Node ID of the left children of the node
+        :param children_right: Node ID of the right children of the node
+        :param decisions: List that will store the decisions of every node in its corresponding index
+        :param stack: List that acts like a stock to store the decisions
+        :param node_id: Node ID (0 if the node is the root node)
+        :param are_leaves: Boolean list that indicates if a node is a leaf
+        """
+        feature = self.tree.feature
+        threshold = self.tree.threshold
+        impurity = self.tree.impurity
+        samples = self.tree.n_node_samples
+        value = self.tree.value
+
+        left_node = children_left[node_id]
+        right_node = children_right[node_id]
+        decisions.insert(node_id, stack)
+        value_neg = value[node_id][0][0]
+        value_pos = value[node_id][0][1]
+        class_str = 'Negative' if value_neg > value_pos else 'Positive'
+        if left_node != right_node:
+            stack_left = stack + [
+                {'Feature': int(feature[node_id]), 'Operator': '<=', 'Threshold': threshold[node_id],
+                 'Impurity': impurity[node_id], 'Samples': samples[node_id], 'Number_Neg': value[node_id][0][0],
+                 'Number_Pos': value[node_id][0][1], 'Class': class_str}
+            ]
+            stack_right = stack + [
+                {'Feature': int(feature[node_id]), 'Operator': '>', 'Threshold': threshold[node_id],
+                 'Impurity': impurity[node_id], 'Samples': samples[node_id], 'Number_Neg': value[node_id][0][0],
+                 'Number_Pos': value[node_id][0][1], 'Class': class_str}
+            ]
+            self.__get_decisions(children_left, children_right, decisions, stack_left, left_node, are_leaves)
+            self.__get_decisions(children_left, children_right, decisions, stack_right, right_node, are_leaves)
+        else:
+            are_leaves[node_id] = True
+            stack += [
+                {'Impurity': impurity[node_id], 'Samples': samples[node_id], 'Number_Neg': value_neg,
+                 'Number_Pos': value_pos, 'Class': class_str}
+            ]
+            decisions.insert(node_id, stack)
